2853bd0972036a4f721fc7623525e3be2fcca7a2
Shoaib Khan <shb.khan@gmail.com>
2015-08-26 19:36:23 -0400

    update SimpleNotification

--- Projects/SimpleNotification/app/src/main/java/ca/shoaib/simplenotification/MainActivity.java
+++ Projects/SimpleNotification/app/src/main/java/ca/shoaib/simplenotification/MainActivity.java
@@ -1,9 +1,14 @@
 package ca.shoaib.simplenotification;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getString(R.string.creator_title));
     }
 
     @Override
@@ -33,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createNotification(View view) {
+        // Prepare result intent
+        Intent intent = new Intent(this, ViewNotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        // Build notification
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(this)
+                        .setColor(getApplicationContext().getResources().getColor(R.color.notification_background))
+                        .setContentTitle(getString(R.string.notification_title))
+                        .setContentText(getString(R.string.notification_content))
+                        .setSmallIcon(android.R.drawable.ic_menu_gallery)
+                        .setAutoCancel(true)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification.build());
     }
 }
