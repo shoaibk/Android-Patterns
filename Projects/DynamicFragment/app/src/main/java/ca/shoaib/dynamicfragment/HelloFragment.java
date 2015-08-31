c0927e20a79cb5137aa16ed6ed302c70aa781666
Shoaib Khan <shb.khan@gmail.com>
2015-08-30 20:24:56 -0400

    Update

---
+++ Projects/DynamicFragment/app/src/main/java/ca/shoaib/dynamicfragment/HelloFragment.java
@@ -0,0 +1,25 @@
+package ca.shoaib.dynamicfragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HelloFragment extends Fragment {
+
+    public HelloFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hello, container, false);
+    }
+}
